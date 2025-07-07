7e3a82a80b93fcfdd3fb2afbb8e7d0eb578c0e0d
Sasha <alexzedim@gmail.com>
2025-07-07 14:48:32 +0300

    build: github-pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next'
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isGithubPages ? '/temple-five-dawns' : '',
+  assetPrefix: isGithubPages ? '/temple-five-dawns/' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
